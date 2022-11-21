c4a2a0a0c4a72a2d17b4196da92f454a82415cc9
liqiujiong <pelle@live.cn>
2022-11-22 00:36:34 +0800

    style: 修改显示样式

--- frontend/src/locales/zh-CN/base/pages.ts
+++ frontend/src/locales/zh-CN/base/pages.ts
@@ -1,5 +1,5 @@
 export default {
-  'pages.layouts.userLayout.title': '打造影视领域最具影响力的外包派单平台',
+  'pages.layouts.userLayout.title': '基于 FastApi + UmiJS 实现了 RBAC 权限管理的全栈式快速开发脚手架',
   'pages.login.accountLogin.tab': '账户密码登录',
   'pages.login.accountLogin.errorMessage': '错误的用户名和密码',
   'pages.login.failure': '登录失败，请重试！',
